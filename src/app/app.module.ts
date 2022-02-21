16a0a224cdf0088e2c049f6d15c990cecbeb308c
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2022-02-21 09:57:04 -0300

    Continuação da componentização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { FullBannerComponent } from './shared/full-banner/full-banner.component';
+import { ListTitlesComponent } from './shared/list-titles/list-titles.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { TitleComponent } from './shared/title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FullBannerComponent
+    FullBannerComponent,
+    ListTitlesComponent,
+    FooterComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
